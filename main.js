6fff19276311475ebeef00844788e466fab7b96c
Kristiana Leka <63458911+Kristiana12@users.noreply.github.com>
2022-04-18 12:38:16 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -7,21 +7,18 @@ $(document).ready(function () {
     $('#main-nav-list').slideToggle('slow');
   });
 
-  const sentenceEL = document.querySelector('.page-title');
-  const sentence = sentenceEL.textContent;
-  const letters = sentence.split('');
-  sentenceEL.textContent = '';
+  function createSpan() {
+    if (window.innerWidth > 850) {
+      const sentenceEL = document.querySelector('.page-title');
+      const sentence = sentenceEL.textContent;
+      const letters = sentence.split('');
+      sentenceEL.textContent = '';
 
-  for (let i = 0; i < letters.length; i++) {
-    sentenceEL.innerHTML += `<span class='jello'> ${letters[i]} </span>`;
+      for (let i = 0; i < letters.length; i++) {
+        sentenceEL.innerHTML += `<span class='jello'> ${letters[i]} </span>`;
+      }
+    }
   }
 
-  // document.querySelectorAll('.jello').forEach((span) => {
-  //   span.addEventListener('mouseover', () => {
-  //     span.classList.add('animateLetters');
-  //   });
-  //   span.addEventListener('mouseleave', () => {
-  //     span.classList.remove('animateLetters');
-  //   });
-  // });
+  createSpan();
 });
